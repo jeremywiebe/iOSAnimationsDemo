6be1c2118b61cad2a737c8beca8bc19d9415d951
Jeremy Wiebe <jeremy@mobify.com>
2015-11-03 19:50:23 -0800

    Initial Commit

---
+++ Animations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Animations
+//
+//  Created by Jeremy Wiebe on 2015-11-03.
+//  Copyright © 2015 Mobify. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
